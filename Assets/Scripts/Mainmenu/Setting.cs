d84874ad1c464ecb903cb702af652830e08db0e0
luoyifengyin <326541148@qq.com>
2020-04-22 00:10:23 +0800

    # 增加了设置界面

---
+++ Assets/Scripts/Mainmenu/Setting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MyGameApplication.MainMenu {
+    public class Setting : MonoBehaviour {
+        public static Setting Instance { get; private set; }
+
+        private Animator m_Animator;
+
+        private void Awake() {
+            Instance = this;
+            m_Animator = GetComponent<Animator>();
+        }
+
+        public void OpenWindow() {
+            m_Animator.SetBool("Open", true);
+        }
+        public void CloseWindow() {
+            m_Animator.SetBool("Open", false);
+        }
+
+        [SerializeField] private Slider m_AudioSlider;
+        [SerializeField] private Text m_VolumeText;
+
+        public void OnSoundChange() {
+            AudioListener.volume = (m_AudioSlider.value - m_AudioSlider.minValue)
+                / (m_AudioSlider.maxValue - m_AudioSlider.minValue);
+            m_VolumeText.text = "" + (int)(AudioListener.volume * 100);
+        }
+    }
+}
