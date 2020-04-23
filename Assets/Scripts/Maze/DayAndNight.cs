3df9343fa7cc1c3dd77efaef8c6358aed43c727d
SaikaFF <463319897@qq.com>
2020-04-23 15:44:23 +0800

    添加初始密室

---
+++ Assets/Scripts/Maze/DayAndNight.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayAndNight : MonoBehaviour
+{
+    float speed = 0.05f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.InvokeRepeating("Round", 1.0f, 1.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.Rotate(Vector3.right * speed);
+    }
+
+    //void Round()
+    //{
+    //    this.transform.Rotate(Vector3.right * speed);
+    //}
+
+}
