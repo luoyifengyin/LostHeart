b7dfc6ae1f20792cfa9f7beed8c47814e1d15001
SaikaFF <463319897@qq.com>
2020-04-27 23:29:13 +0800

    将第三关的地图箭头改为UI。

---
+++ Assets/Scripts/Maze/MiniMapArrow.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapArrow : MonoBehaviour
+{
+    public Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void LateUpdate()
+    {
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, target.eulerAngles.z));
+    }
+}
