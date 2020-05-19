a0ff088909e843ff3be3a26f00ce4338dacefcd3
luoyifengyin <326541148@qq.com>
2020-05-20 00:39:26 +0800

    # 初始房间一点小更新

---
+++ Assets/Scripts/UI/App.cs
@@ -0,0 +1,16 @@
+﻿using MyGameApplication.Manager;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+namespace MyGameApplication.UI {
+    public class App : MonoBehaviour {
+        void Start() {
+            GetComponent<Button>().onClick.AddListener(() => {
+                SceneController.LoadScene("CarRacing");
+            });
+        }
+    }
+}
