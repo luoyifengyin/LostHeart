7a74f258066b4f2c9b4d7a4842866fad1b6756d6
SaikaFF <463319897@qq.com>
2020-05-20 17:26:59 +0800

    添加了序章的APP图片，添加了爆炸特效和音效素材，删除了Maze里我做的车预制体。

---
+++ Assets/Scripts/Maze/Countdown.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Countdown : MonoBehaviour
+{
+    public GameObject text;
+    public int TotalTime = 60;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountDown());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    IEnumerator CountDown()
+    {
+        while (TotalTime >= 0)
+        {
+            text.GetComponent<Text>().text = TotalTime.ToString();
+            yield return new WaitForSeconds(1);
+            TotalTime--;
+
+        }
+    }
+}
