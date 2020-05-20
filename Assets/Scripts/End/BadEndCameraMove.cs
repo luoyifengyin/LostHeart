9ad7f5a769f6e55d680b2322e2b45255ef9adc93
SaikaFF <463319897@qq.com>
2020-05-20 18:43:38 +0800

    添加了NormalEnd，完善了三个结局的场景。

---
+++ Assets/Scripts/End/BadEndCameraMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadEndCameraMove : MonoBehaviour
+{
+    float TranslateSpeed = 0.1f;
+    float TranslateSpeedTime = 0.1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TranslateSpeedTime += 0.1f;
+
+        transform.Translate(Vector3.forward * TranslateSpeed);
+        if (TranslateSpeedTime > 80.0f)
+        {
+            transform.Rotate(0, 180, 0);
+            TranslateSpeedTime = 0.1f;
+        }
+    }
+}
---
+++ Assets/Scripts/End/NormalEndCameraMove.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalEndCameraMove : MonoBehaviour
+{
+    int Speed=1;
+    int Timee=0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Timee++;
+        if (Timee <= 1200)
+            Speed = 1;
+        else
+            Speed = -1;
+        if (Timee > 2400)
+        {
+            Timee = 0;
+        }
+        this.transform.Rotate(Vector3.up * Speed * Time.deltaTime, Space.Self);
+    }
+}
